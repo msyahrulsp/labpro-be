f88a73a711edb91e02abbedbc689a086979f32c1
msyahrulsp <mssp892@gmail.com>
2022-07-29 06:51:18 +0700

    Full route done? & Add jwt

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,31 @@
 import { Express } from 'express'
 import { getHistroy } from './controllers/HistoryController';
-import { getUser } from './controllers/UserController';
+import { getAllUser } from './controllers/UserController';
 import { getVerifAkun } from './controllers/VerifikasiAkunController';
+import { loginHandler } from './controllers/AuthController';
 
 const routes = (app: Express) => {
   app.route('/').get((_, res) => {
     res.status(200).json({
       message: 'API is Working',
+      data: []
     })
   })
-  app.route('/user').get(getUser);
+  app.route('/login').post(loginHandler);
+  app.route('/register').post();
+  app.route('/transfer').post();
   app.route('/verifikasi/akun').get(getVerifAkun);
   app.route('/verifikasi/request').get(getHistroy);
+  app.route('/history').get();
+  app.route('/history/:username').get();
+  app.route('/history').post();
+  app.route('/users').get(getAllUser);
+  app.route('*').get((_, res) => {
+    res.status(404).json({
+      message: 'Path Not Found',
+      data: []
+    })
+  })
 }
 
 export default routes;
